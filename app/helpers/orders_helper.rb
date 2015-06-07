58842eeac0956a590aea27ba34a89d74cd144fb1
sdlong <sdlong.jeng@gmail.com>
2015-06-08 01:59:14 +0800

    Can create payment for ATM, CVS or Creadit card

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,46 @@
 module OrdersHelper
+  def render_payment_service_button(order, type)
+
+    payment_service_for order,
+      order.email,
+      service: :allpay,
+      html: { authenticity_token: false, enforce_utf8: false, method: :post } do |service|
+
+      service.merchant_trade_no   "#{order.id}s#{Time.now.strftime("%Y%m%d%H%M%S")}"
+      service.merchant_trade_date order.created_at
+      service.total_amount        order.price
+      service.trade_desc          "order_sample"
+      service.item_name           "order_sample"
+      service.choose_payment      type_payment(type)
+      service.client_back_url     root_url
+      service.return_url          notify_order_url(order)
+      service.encrypted_data
+
+      submit_tag type_message(type), name: nil
+    end
+  end
+
+  private
+
+  def type_message(type)
+    case type
+    when "credit_card"
+      "信用卡付款"
+    when "cvs"
+      "超商付款"
+    when "atm"
+      "ATM 付款"
+    end
+  end
+
+  def type_payment(type)
+    case type
+    when "credit_card"
+      ActiveMerchant::Billing::Integrations::Allpay::PAYMENT_CREDIT_CARD
+    when "cvs"
+      ActiveMerchant::Billing::Integrations::Allpay::PAYMENT_CVS
+    when "atm"
+      ActiveMerchant::Billing::Integrations::Allpay::PAYMENT_ATM
+    end
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ActiveRecord::Base
+  validates :buyer, :email, :price, presence: true
+
+  def paid?
+    paid
+  end
 end
